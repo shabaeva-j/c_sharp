827a8908373c7dadc60a7d6b17c4c0ca1174f0c7
julia.shabaeva <shabaeva_y@inbox.ru>
2022-08-07 14:49:04 +0300

    task 8

---
+++ task 8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число N, а на выходе покащывет все четные числа от 1 до N
+Console.Write ("Введите число ");
+int number = Convert.ToInt32 (Console.ReadLine()); 
+int numberX = 1; 
+
+while (numberX <= number)
+{if (numberX % 2 == 0) 
+  numberX++;
+ Console.WriteLine (numberX + ",");
+  numberX++ ;
+}
