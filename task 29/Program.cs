363c9c6982a5fc946e4da985aa8e50c2f1dc81a3
julia.shabaeva <shabaeva_y@inbox.ru>
2022-08-19 14:17:00 +0300

    task 29

---
+++ task 29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+int Array = ReadInt("Введите массив чисел: ");
+
+int[] randomArray = new int[Array];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
