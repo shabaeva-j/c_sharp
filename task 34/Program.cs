649bfd3df23c467cd68cab6a0434db16be5c3260
julia.shabaeva <shabaeva_y@inbox.ru>
2022-08-29 16:11:48 +0300

    task 34 new version

--- task 34/Program.cs
+++ task 34/Program.cs
@@ -1,15 +1,17 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.WriteLine($"Введите массив чисел:\n");
+Console.WriteLine($"Введите массив чисел: ");
 int[] numbers = new int[10];
 
 void FillArray(int[] array, int min, int max){
   for (int i = 0; i<array.Length; i++ ){
-    array[i] = new Random().Next(min, max);
+    array[i] = new Random().Next(100,1000);
   }
 }
 
-void WriteArray(int[] array){
+void WriteArrayRandom(int[] array)
+{
     for (int i = 0; i<array.Length; i++ ){
     Console.Write(array[i] + " ");
   }
@@ -19,7 +21,7 @@ void WriteArray(int[] array){
 int QuantityPositive(int[] array){
     int count = 0;
     for (int i = 0; i<array.Length; i++ ){
-    if (array[i] % 2 == 1)
+    if (array[i] % 2 == 0)
     {
       count++;
     }
@@ -28,8 +30,8 @@ int QuantityPositive(int[] array){
 }
 
 FillArray(numbers, 100, 1000);
-WriteArray(numbers);
+WriteArrayRandom(numbers);
 Console.WriteLine();
 
 int count= QuantityPositive(numbers);
-Console.WriteLine($"Количество чётных чисел в массиве: {count}");
+Console.WriteLine($"Количество чётных чисел в массиве составляет: {count}");
